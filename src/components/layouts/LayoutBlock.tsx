20653e54e985e432f754d017c922007d300b621a
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-01-19 23:58:35 +0200

    remove useless code

--- src/components/layouts/LayoutBlock.tsx
+++ src/components/layouts/LayoutBlock.tsx
@@ -1,55 +1,20 @@
 import React, {useState} from 'react';
 import './testStyles/testStyles.css'
 // import {Resizable } from 'react-resizable'
-import {Box} from "@mui/material";
 import { Resizable } from "re-resizable";
 
-/*
-*
-* import { Resizable } from "re-resizable";
-
-* <Resizable
-    style={style}
-    defaultSize={{
-      width: 200,
-      height: 200
-    }}
-  >
-    001
-  </Resizable>
-*
-* */
 
 const items: number[] = [1,2,3,4,5,6]
 
 const LayoutBlock = () => {
-
-    // const MyHandle = React.forwardRef((props, ref) => {
-    //     const {handleAxis, ...restProps} = props;
-    //     return <div ref={ref} className={`foo handle-${handleAxis}`} {...restProps} />;
-    // });
-
-    // <Box className="grid-container" my={2}>
-    //     {items.map((item, index) => (
-    //         <div className="item" key={index}>{item}</div>
-    //     ))}
-    // </Box>
-
-    const style = {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        border: "solid 1px #ddd",
-        background: "#f0f0f0"
-    };
 
     return (
         <Resizable
             className="grid-container"
             // style={style}
             defaultSize={{
-                width: 200,
-                height: 200
+                width: 600,
+                height: 300
             }}
         >
             {items.map((item, index) => (
